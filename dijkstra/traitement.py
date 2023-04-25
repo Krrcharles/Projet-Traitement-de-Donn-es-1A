aba8d4829c9e1d0e0ee6cdce38265bd0a9e2ec7d
Charles <charleswcarrere@gmail.com>
2023-04-25 16:35:21 +0200

    fesfes

--- dijkstra/traitement.py
+++ dijkstra/traitement.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv("data/tarifs-tgv-inoui-ouigo.csv", sep= ";")
-
-print(data.iat[0,1])
-print(data.head())
+class Traitement:
+    
+    def __init__(self, df):
+        pass
+    #grkj
+    
