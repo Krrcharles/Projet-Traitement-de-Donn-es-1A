f41408d5912e63ed1216a0d1078bfd3d374a136d
Stuart_Benoliel <flo.foessel@gmail.com>
2023-05-20 18:13:23 +0200

    small changes

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -4,97 +4,43 @@ import pytest
 from dijkstra.dijkstra import Dijkstra
 
 
-@pytest.mark.parametrize(
-'graphe, source, message_erreur, type_erreur',[
-({'Ville A': ['Ville B', 10.5, 'Ville C', 5.2],
-'Ville B': [('Ville A', 10.5), ('Ville C', 3.1)],
-'Ville C': [('Ville A', 5.2), ('Ville B', 3.1)]},
-'Ville A', "'graphe' n'est pas bien défini", TypeError),
-(['Ville A', [('Ville B', 10.5), ('Ville C', 5.2)],
-'Ville B', [('Ville A', 10.5), ('Ville C', 3.1)],
-'Ville C', [('Ville A', 5.2), ('Ville B', 3.1)]],
-'Ville B', "'graphe' n'est pas bien défini", TypeError),
-({'Paris': [('Lyon', 4), ('Rennes', 3)],
-'Marseille': [('Lyon', 2), ('Rennes', 4),('Isnatabul')],
-'Istanbul' : None ,
-'Lyon': [('Marseille', 2),('Paris', 4), ('Rennes', 3)],
-'Rennes': [('Lyon', 3), ('Marseille', 4),('Paris', 3)]},
-'Lyon', "'graphe' n'est pas bien défini", TypeError),
-({'Paris': [('Lyon', 4), ('Rennes', 3)],
-'Marseille': [('Lyon', 2), ('Rennes', 4),('Isnatabul')],
-'Ajaccio' : [],
-'Lyon': [('Marseille', 2),('Paris', 4), ('Rennes', 3)],
-'Rennes': [('Lyon', 3), ('Marseille', 4),('Paris', 3)]},
-'Istanbul', "'source' n'est pas dans le graphe", ValueError),
-(['Ville A', [('Ville B', 10.5), ('Ville C', 5.2)],
-'Ville B', [('Ville A', 10.5), ('Ville C', 3.1)],
-'Ville C', [('Ville A', 5.2), ('Ville B', 3.1)]],
-5.2, "'source' n'est pas dans le graphe", ValueError),
-(['Ville A', [('Ville B', 10.5), ('Ville C', 5.2)],
-'Ville B', [('Ville A', '3'), ('Ville C', 3.1)],
-'Ville C', [('Ville A', 5.2), ('Ville B', 3.1)]],
-'Ville A', "Les poids associés aux arrêtes ne sont pas des nombres réels", TypeError),
-(['Ville A', [('Ville B', 10.5), ('Ville C', 5.2)],
-'Ville B', [('Ville A', -3), ('Ville C', 3.1)],
-'Ville C', [('Ville A', 5.2), ('Ville B', 3.1)]],
-'Ville A', "Les poids associés aux arrêtes ne sont pas des réels strictement positifs", ValueError)])
+@pytest.mark.parametrize('''dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+ colonne_distance, message_erreur, type_erreur''', [
+  (),()])
 
-def test_erreur_partout_dijkstra(graphe, source, message_erreur, type_erreur):
+def test_erreur_init_dijkstra(dataf, colonne_noeud_depart,
+                              colonne_noeud_arrivee, colonne_distance,
+                              message_erreur, type_erreur):
     with pytest.raises(type_erreur, match=re.escape(message_erreur)):
-        Dijkstra(graphe, source).chemin_partout()
+        Dijkstra(dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+                 colonne_distance)
 
-@pytest.mark.parametrize(
-'graphe, source, resultat_attendu',
-[({'Ville A', [('Ville B', 10.5), ('Ville C', 5.2)],
-'Ville B', [('Ville A', 10.5), ('Ville C', 3.1)],
-'Ville C', [('Ville A', 5.2), ('Ville B', 3.1)]},'Ville A',
-{'Ville B': [['Ville A', 'Ville C', 'Ville B'], 8.3], 'Ville C': [['Ville A', 'Ville C'], 5.2], 'Ville D': [['Ville A', 'Ville D'], 8.7]}),
-({'Paris': [('Lyon', 4), ('Rennes', 3)],
-'Marseille': [('Lyon', 2), ('Rennes', 4),('Isnatabul')],
-'Ajaccio' : [],
-'Lyon': [('Marseille', 2),('Paris', 4), ('Rennes', 3)],
-'Rennes': [('Lyon', 3), ('Marseille', 4),('Paris', 3)]},'Marseille',
-{'Paris': [['Marseille', 'Ville C', 'Ville B'], 8.3], 'Ville C': [['Ville A', 'Ville C'], 5.2], 'Ville D': [['Ville A', 'Ville D'], 8.7]}),
-([2], 2),
-([1, 2], 2),
-([2, 3, 4], 24),
-([-3, 6, 5], -90),
-([0, 2, 4, 7, 12], 0),
-([12, 12], 144),
-([3, 3, 3], 27),
-([2, 2, 2, 2, 2, 2], 64),
-([1, 2, 3, 4, 5, 6], 720),
-([1, -2, 3, -4, 5, -6], -720)]
-)
-def test_resultat_partout_dijkstra(graphe, source, resultat_attendu):
-    assert Dijkstra(graphe, source).chemin_partout() == resultat_attendu
+dataf_ex =
+colonne_noeud_depart_ex =
+colonne_noeud_arrivee_ex =
+colonne_distance_ex =
 
-@pytest.mark.parametrize(
-'graphe, source, destination, message_erreur',[
-({'Ville A': ['Ville B', 10.5, 'Ville C', 5.2],
-'Ville B': [('Ville A', 10.5), ('Ville C', 3.1)],
-'Ville C': [('Ville A', 5.2), ('Ville B', 3.1)]},
-'Ville A', 'Ville A', "'destination' est la source"),
-({'Paris': [('Lyon', 4), ('Rennes', 3)],
-'Marseille': [('Lyon', 2), ('Rennes', 4),('Isnatabul')],
-'Ajaccio' : [],
-'Lyon': [('Marseille', 2),('Paris', 4), ('Rennes', 3)],
-'Rennes': [('Lyon', 3), ('Marseille', 4),('Paris', 3)]},
-'Paris', 'Istanbul', "'destination' n'est pas dans le graphe"),
-({'Paris': [('Lyon', 4), ('Rennes', 3)],
-'Marseille': [('Lyon', 2), ('Rennes', 4),('Isnatabul')],
-'Ajaccio' : [],
-'Lyon': [('Marseille', 2),('Paris', 4), ('Rennes', 3)],
-'Rennes': [('Lyon', 3), ('Marseille', 4),('Paris', 3)]},
-'Paris', Marseille, "'destination' n'est pas dans le graphe")])
+@pytest.mark.parametrize('source, message_erreur', [
+  (),()])
 
-def test_erreur_destination_dijkstra(graphe, source, destination, message_erreur):
+
+def test_erreur_partout_dijkstra(source, message_erreur):
     with pytest.raises(ValueError, match=re.escape(message_erreur)):
-        Dijkstra(graphe, source).chemin_destination(destination)
+        Dijkstra(dataf_ex, colonne_noeud_depart_ex, colonne_noeud_arrivee_ex,
+                 colonne_distance_ex).chemin_partout(source)
 
-@pytest.mark.parametrize(
-'graphe, source, destination, resultat_attendu',
-[([], 1),
+@pytest.mark.parametrize('source, destination, message_erreur', [
+  (),()])
+
+
+def test_erreur_destination_dijkstra(source, destination, message_erreur):
+    with pytest.raises(ValueError, match=re.escape(message_erreur)):
+        Dijkstra(dataf_ex, colonne_noeud_depart_ex, colonne_noeud_arrivee_ex,
+                 colonne_distance_ex).chemin_destination(source, destination)
+
+@pytest.mark.parametrize('''dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+ colonne_distance, message_erreur, type_erreur''', [
+([], 1),
 ([1], 1),
 ([2], 2),
 ([1, 2], 2),
@@ -108,8 +54,31 @@ def test_erreur_destination_dijkstra(graphe, source, destination, message_erreur
 ([1, -2, 3, -4, 5, -6], -720)]
 )
 
-def test_resultat_destination_dijkstra(graphe, source, destination, resultat_attendu):
-    assert Dijkstra(graphe, source).chemin_destination(destination) == resultat_attendu
+def test_resultat_graph_dijkstra(dataf, colonne_noeud_depart,
+                                 colonne_noeud_arrivee, colonne_distance,
+                                 resultat):
+    assert Dijkstra(dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+                    colonne_distance).graph() == resultat
+
+@pytest.mark.parametrize('''dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+ colonne_distance, message_erreur, type_erreur''', [
+  (),()])
+
+
+def test_resultat_partout_dijkstra(source, resultat):
+    assert Dijkstra(dataf_ex, colonne_noeud_depart_ex, colonne_noeud_arrivee_ex,
+                    colonne_distance_ex).chemin_partout(source) == resultat
+
+@pytest.mark.parametrize('''dataf, colonne_noeud_depart, colonne_noeud_arrivee,
+ colonne_distance, message_erreur, type_erreur''', [
+  (),()])
+
+
+def test_resultat_destination_dijkstra(source, destination, resultat):
+    assert (Dijkstra(dataf_ex, colonne_noeud_depart_ex, colonne_noeud_arrivee_ex,
+                     colonne_distance_ex).chemin_destination(source, destination)
+            == resultat)
+
 
 
 graphe = {'Paris': [('Lyon', 4), ('Rennes', 3)],
@@ -124,7 +93,7 @@ graph2 = {
     'Ville D': [('Ville A', 8.7), ('Ville C', 6.8)]
 }
 
-source = 'Paris'
+source1 = 'Paris'
 source2 = 'Ville A'
 A = Dijkstra(graph2, source2)
 print(A.chemin_partout())
